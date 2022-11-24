d1bc689f16d37022c4d6174b93ad854c13b4d92f
Lassi Piironen <lsipii@kapsi.fi>
2022-11-24 19:34:56 +0200

    fix: allow bodyless request

--- src/lib/api_app/src/api/routes/testbed/productizers/user.rs
+++ src/lib/api_app/src/api/routes/testbed/productizers/user.rs
@@ -15,22 +15,18 @@ pub async fn fetch_user_profile(
     request: ParsedRequest,
 ) -> Result<APIRoutingResponse, APIRoutingError> {
     let endpoint_url = "https://gateway.testbed.fi/test/lassipatanen/User/Profile?source=access_to_finland";
-    let request_input = serde_json::from_str(request.body.as_str())?;
     let request_headers = parse_testbed_request_headers(request)?;
-    return post_json_request(endpoint_url, request_input, request_headers).await;
+    return post_json_request(endpoint_url, request_headers).await;
 }
 
 async fn post_json_request(
     endpoint_url: &str,
-    request_input: JSONValue,
     request_headers: HeaderMap,
 ) -> Result<APIRoutingResponse, APIRoutingError> {
-    log::debug!("Input: {:#?}", request_input);
     log::debug!("Headers: {:#?}", request_headers);
 
     let response = reqwest::Client::new()
         .post(endpoint_url)
-        .json(&request_input)
         .headers(request_headers)
         .send()
         .await?;
