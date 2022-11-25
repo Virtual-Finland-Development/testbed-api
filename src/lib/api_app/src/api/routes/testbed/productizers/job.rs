215ec570137ace606e1d2aee11c09c216bf65382
Lassi Piironen <lsipii@kapsi.fi>
2022-11-25 12:13:21 +0200

    fix: input reading

--- src/lib/api_app/src/api/routes/testbed/productizers/job.rs
+++ src/lib/api_app/src/api/routes/testbed/productizers/job.rs
@@ -1,11 +1,11 @@
 use http::StatusCode;
 use log;
 use reqwest;
-use serde::{Deserialize, Serialize};
+use serde::{ Deserialize, Serialize };
 use futures::future;
 
-use crate::api:: {
-    response_types::{APIRoutingError, APIRoutingResponse, ParsedRequest},
+use crate::api::{
+    response_types::{ APIRoutingError, APIRoutingResponse, ParsedRequest },
     routes::application::resolve_external_service_bad_response,
     utils::get_default_headers,
     text_utils::split_text_keep_right,
@@ -20,14 +20,14 @@ struct JobPostingResponse {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-#[allow(non_snake_case)] 
+#[allow(non_snake_case)]
 struct JobPosting {
     employer: String,
     location: Location,
     basicInfo: BasicInfo,
     publishedAt: String,
     applicationEndDate: String,
-    applicationUrl: String
+    applicationUrl: String,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -48,11 +48,13 @@ struct BasicInfo {
  * Get job postings
  */
 pub async fn find_job_postings(
-    request: ParsedRequest,
+    request: ParsedRequest
 ) -> Result<APIRoutingResponse, APIRoutingError> {
-    let request_input = serde_json::from_str(request.body.as_str())?;
+    let request_input = serde_json::from_str::<serde_json::Value>(request.body.as_str())?;
     let request_headers = parse_testbed_request_headers(request)?;
-    let endpoint_urls = vec!["https://gateway.testbed.fi/test/lassipatanen/Job/JobPosting?source=tyomarkkinatori"];
+    let endpoint_urls = vec![
+        "https://gateway.testbed.fi/test/lassipatanen/Job/JobPosting?source=tyomarkkinatori"
+    ];
 
     log::debug!("Input: {:#?}", request_input);
     log::debug!("Headers: {:#?}", request_headers);
@@ -60,21 +62,23 @@ pub async fn find_job_postings(
     // Get the job postings from the external services using concurrent requests and merge them
     // @see: https://stackoverflow.com/a/51047786
     let client = reqwest::Client::new();
-    let response_json_bodies = future::join_all(endpoint_urls.into_iter().map(|endpoint_url| {
-        let client = &client;
-        let headers = request_headers.clone();
-        async move {
-            let source = split_text_keep_right(endpoint_url, "?source=");
-            let response = client
-            .post(endpoint_url)
-            .json(&request_input)
-            .headers(headers)
-            .send()
-            .await?;
-            log::debug!("Source: {source}, Response: {:#?}", response);
-            response.json::<JobPostingResponse>().await
-        }
-    })).await;
+    let response_json_bodies = future::join_all(
+        endpoint_urls.into_iter().map(|endpoint_url| {
+            let client = &client;
+            let headers = request_headers.clone();
+            let input = request_input.clone();
+            async move {
+                let source = split_text_keep_right(endpoint_url, "?source=");
+                let response = client
+                    .post(endpoint_url)
+                    .json(&input)
+                    .headers(headers)
+                    .send().await?;
+                log::debug!("Requesting from source: {source}..");
+                response.json::<JobPostingResponse>().await
+            }
+        })
+    ).await;
 
     // Merge the good responses
     // If any response failed, all fail
@@ -84,7 +88,7 @@ pub async fn find_job_postings(
         results: vec![],
         totalCount: 0,
     };
-    
+
     for r in response_json_bodies {
         match r {
             Ok(r) => {
@@ -92,20 +96,25 @@ pub async fn find_job_postings(
                 let mut results = r.results;
                 response_output.results.append(&mut results);
                 response_output.totalCount += r.totalCount;
-            },
+            }
             Err(r) => {
                 eprintln!("Got an error: {}", r);
                 response_status = r.status().unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
                 error_response_body = r.to_string();
                 break;
-            },
+            }
         }
     }
 
     if response_status == StatusCode::OK {
-        Ok(APIRoutingResponse::new(response_status, &serde_json::to_string(&response_output)?, get_default_headers()))
+        Ok(
+            APIRoutingResponse::new(
+                response_status,
+                &serde_json::to_string(&response_output)?,
+                get_default_headers()
+            )
+        )
     } else {
         resolve_external_service_bad_response(response_status, error_response_body)
     }
-    
 }
