1f8f71b241b2cf8cbcf71a58e51340973f7d2106
Lassi Piironen <lsipii@kapsi.fi>
2022-12-02 12:51:21 +0200

    fix: rust number conversions rounded the ceil-intent wrongly

--- src/lib/api_app/src/api/routes/testbed/productizers/job/mod.rs
+++ src/lib/api_app/src/api/routes/testbed/productizers/job/mod.rs
@@ -1,11 +1,11 @@
-use std::{cmp::Ordering, collections::hash_map::DefaultHasher, hash::Hasher};
-use http::StatusCode;
+use std::{ cmp::Ordering, collections::hash_map::DefaultHasher, hash::Hasher };
+use http::{ StatusCode, HeaderMap };
 use math::round;
 
 use crate::api::{
     responses::{ APIRoutingError, APIRoutingResponse, resolve_external_service_bad_response },
     requests::request_post_many_json_requests,
-    utils::{get_default_headers, ParsedRequest, cut_string_by_delimiter_keep_right},
+    utils::{ get_default_headers, ParsedRequest, cut_string_by_delimiter_keep_right },
 };
 use super::parse_testbed_request_headers;
 
@@ -25,48 +25,30 @@ use job_models::{
 pub async fn find_job_postings(
     request: ParsedRequest
 ) -> Result<APIRoutingResponse, APIRoutingError> {
-    let request_input = serde_json::from_str::<JobsRequestFromFrontend>(request.body.as_str())?;
-    let request_headers = parse_testbed_request_headers(request)?;
     let endpoint_urls = vec![
         "https://gateway.testbed.fi/test/lassipatanen/Job/JobPosting?source=tyomarkkinatori",
         "https://gateway.testbed.fi/test/lassipatanen/Job/JobPosting?source=jobs_in_finland"
     ];
 
-    // Calc compensated pagination parameters
-    let original_limit = request_input.paging.items_per_page;
-    let compensated_limit = (request_input.paging.items_per_page / (endpoint_urls.len() as i32)) as f64;
-    let mut request_limit = round::ceil(compensated_limit, 1) as i32;
-    if request_limit < 1 {
-        request_limit = 1;
-    }
-    let offset = request_input.paging.page_number * request_limit;
-
-    // Create the request input
-    let productizer_request_input = JobsRequest {
-        query: request_input.query,
-        location: request_input.location,
-        paging: RequestPaging {
-            limit: request_limit,
-            offset: offset,
-        },
-    };
+    let request = construct_productizer_requests(request, endpoint_urls)?;
 
     // Fetch the data
-    let (response_status, good_responses, error_response_body) = request_post_many_json_requests::<JobsRequest, JobPostingResponse<JobPosting>>(
-        endpoint_urls,
-        &productizer_request_input,
-        request_headers,
-        true
-    ).await.expect("Something went wrong with the bulk requests");
+    let (response_status, good_responses, error_response_body) = request_post_many_json_requests::<
+        JobsRequest,
+        JobPostingResponse<JobPosting>
+    >(request.0, &request.1, request.2, true).await.expect(
+        "Something went wrong with the bulk requests"
+    );
 
     if response_status == StatusCode::OK {
-        
         // Transform the good response results for the frontend
         let mut good_results = Vec::<JobPostingForFrontend>::new();
         for mut r in good_responses {
-
             let jobs_source = cut_string_by_delimiter_keep_right(r.2, "?source=");
-            let mut transformed_results = transform_job_posting_results(jobs_source, &mut r.0.results);
+            let mut transformed_results = transform_job_posting_results(
+                jobs_source,
+                &mut r.0.results
+            );
             good_results.append(&mut transformed_results);
         }
 
@@ -79,7 +61,10 @@ pub async fn find_job_postings(
 
         // Return the response
         let response_output = JobPostingResponse {
-            results: good_results.iter().take(original_limit as usize).collect(),
+            results: good_results
+                .iter()
+                .take(request.3 as usize)
+                .collect(),
             total_count: total_count,
         };
 
@@ -95,11 +80,45 @@ pub async fn find_job_postings(
     }
 }
 
+pub fn construct_productizer_requests(
+    request: ParsedRequest,
+    endpoint_urls: Vec<&str>
+) -> Result<(Vec<&str>, JobsRequest, HeaderMap, i32), APIRoutingError> {
+    let request_input = serde_json::from_str::<JobsRequestFromFrontend>(request.body.as_str())?;
+    let request_headers = parse_testbed_request_headers(request)?;
+
+    // Calc compensated pagination parameters
+    let original_limit = request_input.paging.items_per_page;
+    let compensated_limit =
+        f64::from(request_input.paging.items_per_page) / f64::from(endpoint_urls.len() as i32);
+    let rounded_limit = round::ceil(compensated_limit, 0);
+    let mut request_limit = rounded_limit as i32;
+    if request_limit < 1 {
+        request_limit = 1;
+    }
+
+    log::debug!("Request limit: {:?}", request_limit);
+
+    let offset = request_input.paging.page_number * request_limit;
+
+    // Create the request input
+    let jobs_request = JobsRequest {
+        query: request_input.query,
+        location: request_input.location,
+        paging: RequestPaging {
+            limit: request_limit,
+            offset: offset,
+        },
+    };
+
+    return Ok((endpoint_urls, jobs_request, request_headers, original_limit));
+}
+
 /**
  * Merge the job posting results, by mutation
  */
-pub fn merge_job_posting_results(results: &mut Vec::<JobPostingForFrontend>) {
-    results.sort_by(|a, b| job_postings_sort_comparator(a,  b));
+pub fn merge_job_posting_results(results: &mut Vec<JobPostingForFrontend>) {
+    results.sort_by(|a, b| job_postings_sort_comparator(a, b));
     results.dedup_by(|a, b| is_job_postings_the_same(a, b));
 }
 
@@ -118,24 +137,31 @@ fn is_job_postings_the_same(a: &JobPostingForFrontend, b: &JobPostingForFrontend
 /**
  * Transform the job posting results
  */
-pub fn transform_job_posting_results(jobs_source: String, results: &mut Vec::<JobPosting>) -> Vec::<JobPostingForFrontend> {
-    results.into_iter().map(|r| JobPostingForFrontend {
-        id: generate_job_posting_id(r),
-        jobs_source: jobs_source.to_string(),
-        employer: r.employer.clone(),
-        location: r.location.clone(),
-        basic_info: r.basic_info.clone(),
-        published_at: r.published_at.clone(),
-        application_url: r.application_url.clone(),
-        application_end_date: r.application_end_date.clone(),
-    }).collect()
+pub fn transform_job_posting_results(
+    jobs_source: String,
+    results: &mut Vec<JobPosting>
+) -> Vec<JobPostingForFrontend> {
+    results
+        .into_iter()
+        .map(|r| JobPostingForFrontend {
+            id: generate_job_posting_id(r),
+            jobs_source: jobs_source.to_string(),
+            employer: r.employer.clone(),
+            location: r.location.clone(),
+            basic_info: r.basic_info.clone(),
+            published_at: r.published_at.clone(),
+            application_url: r.application_url.clone(),
+            application_end_date: r.application_end_date.clone(),
+        })
+        .collect()
 }
 
 /**
  * Utils
  */
 
-// Generate ID for the job posting 
+// Generate ID for the job posting
+
 fn generate_job_posting_id(job_posting: &JobPosting) -> String {
     let job_now = job_posting.clone();
     let app_url = job_now.application_url.clone();
