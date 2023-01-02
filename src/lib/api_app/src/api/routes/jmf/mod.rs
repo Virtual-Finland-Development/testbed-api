59b6bf9978dd2ca6198b9c486825fd517fd8a529
Lauri Saarela <lauri.saarela@gofore.com>
2023-01-02 09:07:26 +0200

    jmf/fetch_jmf_recommendations - use default headers from api utils

--- src/lib/api_app/src/api/routes/jmf/mod.rs
+++ src/lib/api_app/src/api/routes/jmf/mod.rs
@@ -1,25 +1,25 @@
 use std::env;
 
 use crate::api::{
-    responses::{ APIRoutingError, APIRoutingResponse },
     requests::post_json_request,
-    utils::ParsedRequest,
+    responses::{APIRoutingError, APIRoutingResponse},
+    utils::{get_default_headers, ParsedRequest},
 };
 
 mod models;
-use http::HeaderMap;
-use models::{ RecommendationsRequest, RecommendationsResponse };
+use models::{RecommendationsRequest, RecommendationsResponse};
 
-pub async fn fetch_jmf_recommendations(request: ParsedRequest) -> Result<APIRoutingResponse, APIRoutingError> {
-    let endpoint_url = env
-        ::var("JMF_SKILL_RECOMMENDATIONS_ENDPOINT")
+pub async fn fetch_jmf_recommendations(
+    request: ParsedRequest,
+) -> Result<APIRoutingResponse, APIRoutingError> {
+    let endpoint_url = env::var("JMF_SKILL_RECOMMENDATIONS_ENDPOINT")
         .expect("JMF_SKILL_RECOMMENDATIONS_ENDPOINT must be set");
     let request_input: RecommendationsRequest = serde_json::from_str(request.body.as_str())?;
-    let request_headers = HeaderMap::new();
     let response = post_json_request::<RecommendationsRequest, RecommendationsResponse>(
         endpoint_url,
         &request_input,
-        request_headers
-    ).await?;
+        get_default_headers(),
+    )
+    .await?;
     Ok(response)
 }
