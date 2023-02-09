1bce6fb5c5c4aa1c6ebc9c67dd368eb4508467e5
lassi.patanen <lassi.patanen@gofore.com>
2023-02-09 08:20:32 +0200

    Build data product URI with format! macro

--- src/lib/api_app/src/api/routes/testbed/productizers/person/mod.rs
+++ src/lib/api_app/src/api/routes/testbed/productizers/person/mod.rs
@@ -16,10 +16,10 @@ pub mod basic_information;
 pub mod job_applicant_profile;
 
 fn build_data_product_uri(data_product: &str, data_source: &str) -> String {
-    let testbed_base_url: String = env::var("TESTBED_BASE_URL").expect("TESTBED_BASE_URL must be set");
-    let testbed_environment: String = env::var("TESTBED_ENVIRONMENT").expect("TESTBED_ENVIRONMENT must be set");
-    let path: String = testbed_base_url + data_product + "?source=" + data_source + ":" + testbed_environment.as_str();
-    path
+    let testbed_base_url = env::var("TESTBED_BASE_URL").expect("TESTBED_BASE_URL must be set");
+    let testbed_environment = env::var("TESTBED_ENVIRONMENT").expect("TESTBED_ENVIRONMENT must be set");
+    let formatted_path = format!("{testbed_base_url}/{data_product}?source={data_source}:{testbed_environment}");
+    formatted_path
 }
 
 pub async fn get_data_product(data_product: &str, data_source: &str, request: ParsedRequest) -> Result<APIRoutingResponse, APIRoutingError> {
