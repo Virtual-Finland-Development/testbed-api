9f04b8eb32f669824c186e2c3c912de242f3ad9e
lassi.patanen <lassi.patanen@gofore.com>
2023-02-09 08:24:57 +0200

    Use correct data product for writing basic information

--- src/lib/api_app/src/api/routes/testbed/productizers/person/basic_information/mod.rs
+++ src/lib/api_app/src/api/routes/testbed/productizers/person/basic_information/mod.rs
@@ -11,7 +11,7 @@ pub async fn get_basic_information(request: ParsedRequest) -> Result<APIRoutingR
 }
 
 pub async fn write_basic_information(request: ParsedRequest) -> Result<APIRoutingResponse, APIRoutingError> {
-    let data_product = "draft/Person/BasicInformation";
+    let data_product = "draft/Person/BasicInformation/Write";
     let data_source = "virtualfinland";
     let result = super::write_data_product(data_product, data_source, request).await?;
     Ok(result)
