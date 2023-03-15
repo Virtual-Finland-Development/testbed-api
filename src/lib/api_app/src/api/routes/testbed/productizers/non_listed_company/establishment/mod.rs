e46249e960beaca91f3291502a6648126dec2838
lassi.patanen <lassi.patanen@gofore.com>
2023-03-15 09:49:51 +0200

    Added endpoints for NSG data products

---
+++ src/lib/api_app/src/api/routes/testbed/productizers/non_listed_company/establishment/mod.rs
@@ -0,0 +1,35 @@
+use app::{
+    responses::APIResponse,
+    router::ParsedRequest
+};
+
+#[utoipa::path(
+    post,
+    path = "/testbed/productizer/non-listed-company/establishment",
+    request_body(
+        content = Object,
+        description = "Write company establishment information",
+        examples(( "Success" = (
+            summary = "JSON example",
+            value = json!("Loading..."),
+            external_value = "https://raw.githubusercontent.com/Virtual-Finland/definitions/main/DataProducts/draft/NSG/Agent/NonListedCompany/Establishment/Write.json"
+        )))
+    ),
+    responses((
+        status = 200,
+        body = Object,
+        description = "Company establishment response",
+        examples(( "Success" = (
+            summary = "JSON example",
+            value = json!("Loading..."),
+            external_value = "https://raw.githubusercontent.com/Virtual-Finland/definitions/main/DataProducts/draft/NSG/Agent/NonListedCompany/Establishment/Write.json"
+        )))
+    )),
+    security(( "BearerAuth" = [] ))
+)]
+pub async fn write_establishment(request: ParsedRequest) -> APIResponse {
+    let data_product = "draft/NSG/Agent/LegalEntity/NonListedCompany/Establishment/Write";
+    let data_source = "virtualfinland";
+    let result = super::post_data_product(data_product, data_source, request).await?;
+    Ok(result)
+}
